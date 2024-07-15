b08abbe3c9884c7c465efe74483d448d83c33520
kunalgavhane12 <kunalgavhane12@gmail.com>
2024-07-15 21:38:44 +0530

    Dock

--- Dock/mainwindow.cpp
+++ Dock/mainwindow.cpp
@@ -1,19 +1,157 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QPainter>
+#include <QMouseEvent>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setCentralWidget(ui->dockWidget);
-    QImage *image = new QImage(ui->label->size(),QImage::Format_RGB32);
-    image->fill(Qt::green);
-    ui->label->setPixmap(QPixmap::fromImage(*image));
+
+    image = new QImage(this->size(), QImage::Format_RGB32);
+    image->fill(Qt::white);
+
+    setCentralWidget(centralWidget());
+
+    connect(ui->actionRectangle, &QAction::triggered, this, &MainWindow::setshapeRectangle);
+    connect(ui->actionLine, &QAction::triggered, this, &MainWindow::setshapeLine);
+    connect(ui->actionOval, &QAction::triggered, this, &MainWindow::setshapeCircle);
+
+    shape = None;
+    selectedShape = None;
+    resizing = false;
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete image;
 }
 
+void MainWindow::setshapeRectangle()
+{
+    shape = Rectangle;
+}
+
+void MainWindow::setshapeLine()
+{
+    shape = Line;
+}
+
+void MainWindow::setshapeCircle()
+{
+    shape = Circle;
+}
+
+void MainWindow::paintEvent(QPaintEvent *event)
+{
+    Q_UNUSED(event);
+    QPainter painter(this);
+    drawShapes(painter);
+}
+
+void MainWindow::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        if (shape == Rectangle)
+        {
+            currentRect.setTopLeft(event->pos());
+            currentRect.setBottomRight(event->pos());
+        }
+        else if (shape == Line)
+        {
+            currentLine.setP1(event->pos());
+            currentLine.setP2(event->pos());
+        }
+        else if (shape == Circle)
+        {
+            currentCircle.setTopLeft(event->pos());
+            currentCircle.setBottomRight(event->pos());
+        }
+        update();
+    }
+    dragStartPosition = event->pos();
+}
+
+void MainWindow::mouseMoveEvent(QMouseEvent *event)
+{
+    if (event->buttons() & Qt::LeftButton)
+    {
+        if (shape == Rectangle)
+        {
+            currentRect.setBottomRight(event->pos());
+        }
+        else if (shape == Line)
+        {
+            currentLine.setP2(event->pos());
+        }
+        else if (shape == Circle)
+        {
+            currentCircle.setBottomRight(event->pos());
+        }
+        update();
+    }
+}
+
+void MainWindow::mouseReleaseEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton)
+    {
+        if (shape == Rectangle)
+        {
+            rectangles.append(currentRect.normalized());
+        }
+        else if (shape == Line)
+        {
+            lines.append(currentLine);
+        }
+        else if (shape == Circle)
+        {
+             circles.append(currentCircle);
+        }
+
+        currentRect = QRect();
+        currentLine = QLineF();
+        currentCircle = QRect();
+        update();
+    }
+}
+
+void MainWindow::drawShapes(QPainter &painter)
+{
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.setPen(Qt::black);
+
+    foreach (const QRect &rect, rectangles)
+    {
+        painter.drawRect(rect);
+    }
+
+    foreach (const QLineF &line, lines)
+    {
+        painter.drawLine(line);
+    }
+
+    foreach (const QRect &rect, circles)
+    {
+        painter.drawEllipse(rect);
+    }
+
+    if (!currentRect.isNull() && shape == Rectangle)
+    {
+        painter.drawRect(currentRect);
+    }
+
+    if (!currentLine.isNull() && shape == Line)
+    {
+        painter.drawLine(currentLine);
+    }
+
+    if (!currentCircle.isNull() && shape == Circle)
+    {
+         painter.drawEllipse(currentCircle);
+    }
+}
--- Dock/mainwindow.h
+++ Dock/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QImage>
+#include <QRect>
+#include <QLineF>
+#include <QStack>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -12,10 +16,48 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    enum Shape {
+        None,
+        Rectangle,
+        Line,
+        Circle
+    };
+
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+protected:
+    void paintEvent(QPaintEvent *event) override;
+    void mousePressEvent(QMouseEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void mouseReleaseEvent(QMouseEvent *event) override;
+
+public slots:
+    void setshapeRectangle();
+    void setshapeLine();
+    void setshapeCircle();
+    void drawShapes(QPainter &painter);
+
 
 private:
     Ui::MainWindow *ui;
+    QImage *image;
+    Shape shape;
+    Shape selectedShape;
+
+    QRect currentRect;
+    QLineF currentLine;
+    QRect currentCircle;
+    QPoint dragStartPosition;
+    bool resizing;
+    QPoint resizeStartPos;
+    QRect resizeStartRect;
+
+    QList<QRect> rectangles;
+    QList<QLineF> lines;
+    QList<QRect> circles;
+
+
 };
+
 #endif // MAINWINDOW_H
